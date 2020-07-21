b593719a8c9a08607409051b474c2f407ef5bf90
Jianmin Chen <jianminchen.fl@gmail.com>
2020-07-21 16:41:27 -0700

    check in

---
+++ LongestRepeatedSubstring-overlap/LongestRepeatedSubstring.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longest_repeated_substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunTestcase1();
+            RunTestcase2();
+        }
+
+        /// <summary>
+        /// July 21, 2020
+        /// </summary>
+        public static void RunTestcase1()
+        {
+            longestRepeatedSubstring("ABGABG");
+        }
+
+        /// <summary>
+        /// longest non-overlap repeated substring - "XYXY"
+        /// The algorithm is designed for non-overlap repeated substring, so
+        /// it should return "XYXY".
+        /// 
+        /// longest overlap repeated substring - "XYXYXYXY"
+        /// </summary>
+        public static void RunTestcase2()
+        {
+            var result = longestRepeatedSubstring("XYXYXYXY");
+        }
+
+        /// Longest repeatable substring - 
+        /// https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/
+        /// dynamic programming 
+        /// Dynamic Programming : This problem can be solved in O(n^2) time using Dynamic Programming. 
+        /// The basic idea is to find the longest repeating suffix for all prefixes in the string str.
+        /// 
+        /// To avoid overlapping we have to ensure that the length of suffix is less than (j-i) 
+        /// at any instant.
+        /// The maximum value of LCSRe(i, j) provides the length of the longest repeating substring 
+        /// and the substring itself can be found using the length and the ending index of the common 
+        /// suffix.
+        /// 
+        public static String longestRepeatedSubstring(String str)
+        {
+            int n = str.Length;
+            var LCSRe = new int[n + 1, n + 1];
+
+            var longestSubstring = "";    
+            int maxLength = 0; // To store length of result  
+
+            // building table in bottom-up manner  
+            int start;
+            int index = 0;
+
+            // work on substring [start, end] inclusive
+            for (start = 1; start <= n; start++)
+            {
+                for (int end = start + 1; end <= n; end++)
+                {
+                    var previousStart = start - 1;
+                    var previousEnd = end - 1; 
+
+                    // (end - start) > LCSRe[start - 1][end - 1] to remove overlapping                    
+                    if ( str[previousStart] == str[previousEnd] &&
+                         LCSRe[previousStart, previousEnd] < (end - start))
+                    {
+                        LCSRe[start, end] = LCSRe[start - 1, end - 1] + 1;
+
+                        // updating maximum length of the  
+                        // substring and updating the finishing  
+                        // index of the suffix  
+                        if (LCSRe[start, end] > maxLength)
+                        {
+                            maxLength = LCSRe[start, end];
+                            index = Math.Max(start, index);
+                        }
+                    }
+                    else
+                    {
+                        LCSRe[start, end] = 0;
+                    }
+                }
+            }
+
+            // If we have non-empty result, then insert all  
+            // characters from first character to last  
+            // character of String  
+            if (maxLength > 0)
+            {
+                for (start = index - maxLength + 1; start <= index; start++)
+                {
+                    longestSubstring += str[start - 1];
+                }
+            }
+
+            return longestSubstring;
+        }
+    }
+}
---
+++ LongestRepeatedSubstring-overlap/ReadMe.md
@@ -0,0 +1,42 @@
+July 21, 2020<br>
+
+It is tough learning experience how to solve this algorithm using dynamic programming. <br>
+
+Longest repeatable substring<br>
+[Here](https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/) is the link on geeksforgeeks.com. <br>
+
+The problem statement is here:<br>
+Given a string str, find the longest repeating non-overlapping substring in it. In other words find 2 identical substrings of maximum length which do not overlap. If there exists more than one such substring return any of them.<br>
+
+Case study: <br>
+"ABGABG"<br>
+I like to work on this test case, and explain the idea how to find longest repeated substring. <br>
+Since the substring repeated is not defined in terms of length, start position and end position, so it is easy to figure out that we need to find the dynamic programming using dp[start, end], the value is longest repeated substring. <br>
+
+It is easy to figure out the longest repeated substring is "ABG". The goal is to find a substring with repeated "G", and then a substring with repeated "BG", and then longest one, a substring with repeated "ABG". <br>
+
+The most important is to build a recurrence formula to construct dynamic programming table. <br>
+"ABGABG" <br>
+Add one char at the end of string, '|'
+"ABGABG|"
+ 0123456  <br>
+First the substring "BGAB" has longest repeated substring "B", and then DP[1,4] = 1; <br>
+Next the substring "GABG" has longest repeated substring "BG", and then DP[2,5] = 2, the most important is to understand the recurrence formula: Dp[2,5] = Dp[1,4] + 1, since "ABGABG" at position 1, str[1] = 'B', and str[4] = 'B', so we can argue that "BG" is longest repeated substring. <br>
+
+"BGAB" has the repeated substring, and also first and last char are the same. <br>
+"GABG" has the repeated substring, and also the first and last char are the same. <br>
+
+One thing to argue is that repeated substring ending at last position.<br>
+For example, "ABGABG|". 
+Those are bigger than zero: dp[1,4], dp[2,5], dp[3,6], since previous start and previous end are the same characters. 
+"ABG|"
+DP[3,6] = DP[2,5] + 1 since str[2] == str[5] = 'G', so longest repeated substring is "ABG".  <br>
+
+I am not sure how it works. But I just spent time to debug the code and then wrote down the ideas. <br>
+I will look into more articles related to prefix and suffix. <br>
+
+
+
+
+
+
